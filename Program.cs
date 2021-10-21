5b495beaf2a58cfb1dfd5dab1ff8403c64b9542b
Khalmatov <large000@yandex.ru>
2021-10-22 00:22:27 +0300

    Задание 7.1.4

--- Program.cs
+++ Program.cs
@@ -9,4 +9,21 @@ namespace Module_7
             Console.WriteLine("Hello World!");
         }
     }
+
+    class Employee
+    {
+        public string Name;
+        public int Age;
+        public int Salary;
+    }
+
+    class ProjectManager:Employee
+    {
+        public string ProjectName;
+    }
+
+    class Developer:Employee
+    {
+        string ProgrammingLanguage;
+    }
 }
