bad1bae4177cb9a702c858c077cf621c2bedc232
Khalmatov <large000@yandex.ru>
2021-10-22 00:26:19 +0300

    Задание 7.1.5

---
+++ Class1.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Module_7
+{
+
+
+    class Food
+    {
+
+    }
+
+    class Fruit:Food
+    {
+
+    }
+    class Vegetable:Food
+    {
+
+
+    }
+    class Apple:Fruit
+    {
+    }
+
+    class Banana : Fruit
+    {
+
+    }
+    class Pear : Fruit
+    {
+
+    }
+    class Potato : Vegetable
+    {
+
+    }
+
+    class Carrot : Vegetable
+    {
+
+    }
+}
