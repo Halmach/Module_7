9ae0bbfb43db76ff34287182b0b290f841a2dd2f
Khalmatov <large000@yandex.ru>
2021-10-25 22:49:54 +0300

    Задание 7.2.14

---
+++ IndexingClass.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Module_7
+{
+    class IndexingClass
+    {
+        private int[] array;
+
+        public int this[int i]
+        {
+            get
+            {
+                if (i >= 0 && i < array.Length) return array[i];
+                else return -1;
+            }
+            set
+            {
+                array[i] = value;
+            }
+        }
+        public IndexingClass(int[] array)
+        {
+            this.array = array;
+        }
+       
+    }
+}
