55b5983323d4cc4563d3cabc98fa1ff3755d8134
Khalmatov <large000@yandex.ru>
2021-10-25 23:12:20 +0300

    Задание 7.3.3

---
+++ ComputerPart.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Module_7
+{
+    abstract class ComputerPart
+    {
+        public abstract void Work();
+    }
+
+    class Processor:ComputerPart
+    {
+        public override void Work()
+        {
+            Console.WriteLine("Работает процессор");
+        }
+
+    }
+    class MotherBoard:ComputerPart
+    {
+        public override void Work()
+        {
+            Console.WriteLine("Работает материнская плата");
+        }
+    }
+
+    class GraphicCard:ComputerPart
+    {
+        public override void Work()
+        {
+            Console.WriteLine("Работает видеокарта");
+        }
+    }
+
+
+}
