8649309933629fef38d83968b9f2c5984146f42a
Khalmatov <large000@yandex.ru>
2021-10-28 23:39:00 +0300

    Задание 7.6.6

---
+++ Record.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Module_7
+{
+    class Record <T1,T2>
+    {
+        T1 id;
+        T2 value;
+        DateTime Date;
+    }
+}
