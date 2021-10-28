22ca3d7ab1f84b7ad0120e077243f091e83ac08d
Khalmatov <large000@yandex.ru>
2021-10-28 23:34:00 +0300

    Задание 7.6.2

---
+++ Car.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Module_7
+{
+    class Car<T>
+    {
+        T Engine;
+
+    }
+
+    class ElectricEngine
+    {
+
+    }
+
+    class GasEngine
+    {
+
+    }
+}
