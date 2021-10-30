7f80e5c35dce643d92adef1733a1a0f5897c8d47
Khalmatov <large000@yandex.ru>
2021-10-31 00:41:58 +0300

    проверка изменяется ли строка по ссылке ?

--- Program.cs
+++ Program.cs
@@ -9,12 +9,31 @@ namespace Module_7
             //InheritedClass b = new InheritedClass();
 
             //Console.WriteLine("Hello World!");
-            BaseClass cl = new DerivedClass();
-            ((DerivedClass)cl).Display();
-     
+            //BaseClass cl = new DerivedClass();
+            //((DerivedClass)cl).Display();
+            var str = "Good buy";
+            Console.WriteLine("0 " + str);
+            ChangeString(str);
+            Console.WriteLine("2 " + str);
 
+
+            ProjectManager obj = new ProjectManager();
+
+            if (obj is Employee)
+            {
+                Console.WriteLine("Объект класса Animal");
+            }
+        }
+
+
+        static void ChangeString(string  str)
+        {
+            str = "Hello";
+            Console.WriteLine(str);
         }
     }
+
+
 
     class Employee
     {
